f9f2630027e3e92db5a43103efc5024aba8de5d6
harshada1709 <bagal.h@northeastern.edu>
2022-12-19 19:18:41 -0500

    Added header , footer , nav components

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from './components/Header/header.jsx'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header></Header>
+    </>
   )
 }
 
